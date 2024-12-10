af23c4d2e375776bc3cfe91b055f291cb267f86c
Andrew Nesvadba <andrew.nesvadba@neds.com>
2024-12-11 01:05:27 +1000

    refactor: simplify

--- src/day10/part1.ts
+++ src/day10/part1.ts
@@ -2,16 +2,24 @@
 
 import { Grid } from "@lib/grid";
 
-export function nextLocs(grid: Grid<number>, x: number, y: number): [number, number][] {
-    const current = grid.get(x, y);
-    if (current === undefined) throw new Error(`No current at ${x}, ${y}: ${current}`);
-    const next: [number, number][] = [];
-    if (grid.get(x + 1, y) === current + 1) next.push([x + 1, y]);
-    if (grid.get(x - 1, y) === current + 1) next.push([x - 1, y]);
-    if (grid.get(x, y + 1) === current + 1) next.push([x, y + 1]);
-    if (grid.get(x, y - 1) === current + 1) next.push([x, y - 1]);
+const directions: [number, number][] = [
+    [0, -1],
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+];
 
-    return next;
+export function bindNextLocs(grid: Grid<number>) {
+    return (x: number, y: number): [number, number][] => {
+        const current = grid.get(x, y);
+        if (current === undefined) throw new Error(`No current at ${x}, ${y}`);
+        const next: [number, number][] = [];
+        for (const [dx, dy] of directions) {
+            if (grid.get(x + dx, y + dy) === current + 1) next.push([x + dx, y + dy]);
+        }
+
+        return next;
+    }
 }
 
 export function part1(input: string): number {
@@ -20,23 +28,27 @@ export function part1(input: string): number {
         .split('\n')
         .map((lines) => lines.split('').map(Number));
     const grid = new Grid(items);
+    const getNextLocs = bindNextLocs(grid);
     const starts = grid.find(0);
 
     let result = 0;
     for (const start of starts) {
-        let next = [start];
-        while (next.length > 0) {
-            const found: [number, number][] = [];
-            for (const [x, y] of next) {
+        let nextLocs = [start];
+        while (nextLocs.length > 0) {
+            const foundLocs: [number, number][] = [];
+
+            for (const [x, y] of nextLocs) {
                 if (grid.get(x, y) === 9) result++;
-                else found.push(...nextLocs(grid, x, y));
+                else foundLocs.push(...getNextLocs(x, y));
             }
-            for (const [i, [x, y]] of found.entries()) {
-                for (let j = i + 1; j < found.length; j++) {
-                    if (found[j][0] === x && found[j][1] === y) found[j] = [Number.NaN, Number.NaN];
+
+            for (const [i, [x, y]] of foundLocs.entries()) {
+                for (let j = i + 1; j < foundLocs.length; j++) {
+                    if (foundLocs[j][0] === x && foundLocs[j][1] === y) foundLocs[j] = [Number.NaN, Number.NaN];
                 }
             }
-            next = found.filter(([x, y]) => !Number.isNaN(x) && !Number.isNaN(y));
+
+            nextLocs = foundLocs.filter(([x, y]) => !Number.isNaN(x) && !Number.isNaN(y));
         }
     }
 
--- src/day10/part2.ts
+++ src/day10/part2.ts
@@ -1,7 +1,7 @@
 // Advent of Code - Day 10 - Part Two
 
 import { Grid } from "@lib/grid";
-import { nextLocs } from "./part1";
+import { bindNextLocs } from "./part1";
 
 export function part2(input: string): number {
     const items = input.trim()
@@ -9,18 +9,21 @@ export function part2(input: string): number {
         .split('\n')
         .map((lines) => lines.split('').map(Number));
     const grid = new Grid(items);
+    const getNextLocs = bindNextLocs(grid);
     const starts = grid.find(0);
 
     let result = 0;
     for (const start of starts) {
-        let next = [start];
-        while (next.length > 0) {
-            const found: [number, number][] = [];
-            for (const [x, y] of next) {
+        let nextLocs = [start];
+        while (nextLocs.length > 0) {
+            const foundLocs: [number, number][] = [];
+
+            for (const [x, y] of nextLocs) {
                 if (grid.get(x, y) === 9) result++;
-                else found.push(...nextLocs(grid, x, y));
+                else foundLocs.push(...getNextLocs(x, y));
             }
-            next = found;
+
+            nextLocs = foundLocs;
         }
     }
 
