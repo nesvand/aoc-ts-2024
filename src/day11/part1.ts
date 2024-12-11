77ccd4fc6fefe0e8e3a9fd467c8fdd60e814217e
Andrew Nesvadba <andrew.nesvadba@neds.com>
2024-12-11 21:49:05 +1000

    feat: day11 complete

---
+++ src/day11/part1.ts
@@ -0,0 +1,43 @@
+// Advent of Code - Day 11 - Part One
+
+/*
+    Rules:
+    - If stone equals 0, replace the stone with a value of 1
+    - If the stone has an even number of digits (ie. 12, 1234, 123456, etc.), split the number into two numbers, and replace with two stones of those values
+      - ie. 12 becomes 1 and 2, 1234 becomes 12 and 34, 123456 becomes 123 and 456
+    - If none of the other rules apply, the stone is replaced by a new stone multiplied by 2024, ie. 100 becomes 202400
+    - Order is preserved
+*/
+
+const memo = new Map<string, number>();
+
+export function processStone(stone: number, blinks: number): number {
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    if (memo.has(`${stone},${blinks}`)) return memo.get(`${stone},${blinks}`)!;
+    if (blinks === 0) return 1;
+
+    let total = 0;
+    const stoneString = stone.toString();
+    if (stone === 0) total = processStone(1, blinks - 1);
+    else if (stoneString.length % 2 === 0) {
+        const left = Number.parseInt(stoneString.slice(0, stoneString.length / 2));
+        const right = Number.parseInt(stoneString.slice(-stoneString.length / 2));
+        total = processStone(left, blinks - 1) + processStone(right, blinks - 1);
+    } else {
+        total = processStone(stone * 2024, blinks - 1);
+    }
+    memo.set(`${stone},${blinks}`, total);
+    return total;
+}
+
+export function part1(input: string): number {
+    const stones = input.trim().split(' ').map(Number);
+
+    const blinks = 25;
+    let total = 0;
+    for (const stone of stones) {
+        total += processStone(stone, blinks);
+    }
+
+    return total;
+}
---
+++ src/day11/part2.ts
@@ -0,0 +1,15 @@
+// Advent of Code - Day 11 - Part Two
+
+import { processStone } from "./part1";
+
+export function part2(input: string): number {
+    const stones = input.trim().split(' ').map(Number);
+
+    const blinks = 75;
+    let total = 0;
+    for (const stone of stones) {
+        total += processStone(stone, blinks);
+    }
+
+    return total;
+}
